7aa4d9a3e5e5ae56aab7b5316170fd4b908795b8
MuhammadAbozaid77 <muhammadaozaid77@gmail.com>
2024-09-14 00:23:42 +0300

    fetching movies

--- src/pages/movies/UpComingMovies.jsx
+++ src/pages/movies/UpComingMovies.jsx
@@ -1,3 +1,50 @@
+import { useEffect, useState } from "react";
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNWUxZDFiYzMzZTgxNzc1MGNjNGFlYmE2MTkyZTlmMCIsIm5iZiI6MTcyNjI0Mjc1MS44ODg1OTEsInN1YiI6IjYzNzg4YzE4YzJmNDRiMDBjMzU1MjhmNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wvL8v2w7zbfwr-DVB7gFLN6UXx08p4XUNCym3IrN6cs",
+  },
+};
+const url = "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1";
+
 export default function UpComingMovies() {
-  return <div>UpComingMovies</div>;
+  const [upComingList, setUpComingList] = useState([]);
+  useEffect(() => {
+    async function getUpComingMovies() {
+      const data = await fetch(url, options);
+      const zzz = await data?.json();
+      console.log("zzz", zzz);
+      setUpComingList(zzz?.results);
+    }
+
+    getUpComingMovies();
+  }, []);
+
+  return (
+    <>
+      <div className="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+        {upComingList?.length > 0 &&
+          upComingList?.map((el) => (
+            <div key={el?.id}>
+              <div className="col-span-1 text-white rounded-md overflow-hidden  relative border border-gray-800">
+                <img
+                  className="hover:scale-105 duration-300 transition-all"
+                  src={`https://image.tmdb.org/t/p/original${el?.poster_path}`}
+                  alt=""
+                />
+                {/* <div className="hover:bg-black/40 absolute inset-0"></div> */}
+              </div>
+              <h1 className="text-gray-200 mt-1 text-[20px]">
+                {" "}
+                {el?.original_title}{" "}
+              </h1>
+              <h1 className="text-gray-500"> {el?.vote_average} </h1>
+            </div>
+          ))}
+      </div>
+    </>
+  );
 }
